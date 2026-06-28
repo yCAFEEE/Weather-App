27bd7f3ed0b96f9d68c0db3a3121e0ec694e7233
yCAFEEE <yuridan2705@gmail.com>
2026-06-28 16:22:41 -0300

    add main tag

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -57,32 +57,34 @@ export default function Home(){
         </form>
       </header>
       
-      {weather && (
-        <div className='weather-container'>
-          <h1>{weather.city}</h1>
-          <div className='temp-container'>
-            <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Weather icon" />
-            <h1>{weather.temperature} °C</h1>
+      <main>
+        {weather && (
+          <div className='weather-container'>
+            <h1>{weather.city}</h1>
+            <div className='temp-container'>
+              <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Weather icon" />
+              <h1>{weather.temperature} °C</h1>
+            </div>
+            <div className='desc-container'>
+              <h2>{weather.description}</h2>
+            </div>
+            <h2>Local time: {localHours + ":" + localMinutes}</h2>
+            <div className='weather-overview-container'>
+              <p>Humidity: {weather.humidity}%</p>
+              <p>Feel: {weather.feelsLike} °C</p>
+              <p>Min. temperature: {weather.tempMin} °C</p>
+              <p>Max temperature: {weather.tempMax} °C</p>
+              <p>Pressure: {weather.pressure}</p>
+              {weather.visibility && <p>Visibility: {weather.visibility / 1000}km</p>}
+              <p>Wind speed: {(weather.windSpeed * 3.6).toFixed(2)}km/h</p>
+            </div>
           </div>
-          <div className='desc-container'>
-            <h2>{weather.description}</h2>
-          </div>
-          <h2>Local time: {localHours + ":" + localMinutes}</h2>
-          <div className='weather-overview-container'>
-            <p>Humidity: {weather.humidity}%</p>
-            <p>Feel: {weather.feelsLike} °C</p>
-            <p>Min. temperature: {weather.tempMin} °C</p>
-            <p>Max temperature: {weather.tempMax} °C</p>
-            <p>Pressure: {weather.pressure}</p>
-            {weather.visibility && <p>Visibility: {weather.visibility / 1000}km</p>}
-            <p>Wind speed: {(weather.windSpeed * 3.6).toFixed(2)}km/h</p>
-          </div>
-        </div>
-      )}
+        )}
 
-      {error && (
-        <h2>{error}</h2>
-      )}
+        {error && (
+          <h2>{error}</h2>
+        )}
+      </main>
     </>
   )
 }
